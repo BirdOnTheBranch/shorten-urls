8e402f894ea47813793c16ca20b0caaf1fb3a191
BirdOnTheBranch <Luis.Planelles@gmail.com>
2020-02-29 12:51:25 +0100

    Changes in redirection fiel code in admin, now, only field url is a hiperlink

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,8 @@ from .models import Link
 # Register your models here.
 class AdminLink(admin.ModelAdmin):
     readonly_fields = ('create',)
-    list_display = ('usuario', 'get_url','get_code')
+    list_display = ('usuario', 'get_url','code')
+    list_display_links = ('code',)
     ordering = ('create',)
 
 
@@ -15,13 +16,6 @@ class AdminLink(admin.ModelAdmin):
             return format_html(f"<a href='{obj.url}' target='_blank' >{obj.url}</a>")
 
     get_url.short_description = 'url'
-
-
-    def get_code(self, obj):
-        if obj.code:
-            return format_html(f"<a href='http://127.0.0.1:8000/{obj.code}' target='_blank' >{obj.code}</a>")
-
-    get_code.short_description = 'code'
 
 
 admin.site.register(Link, AdminLink)
